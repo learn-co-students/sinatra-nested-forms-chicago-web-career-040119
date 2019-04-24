61dddb4f0956ed14ad1df6baba8f686440bd7243
diautzi <dia_utzi@yahoo.com>
2019-04-23 21:24:47 -0500

    Done.

--- app.rb
+++ app.rb
@@ -8,9 +8,13 @@ module FormsLab
       erb :root
     end
 
+# In order to serve the form in the browser, we need a GET request:
     get "/new" do
       erb :"pirates/new"
     end
+
+    # And now we need a way to process the input from the user and to display the pirates and their SHIPS.
+     # We process a form with a POST request:
 
      post "/pirates" do
        @pirate = Pirate.new(params[:pirate])
